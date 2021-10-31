12400932b2d8095df5d57eed9ab6afd548ca7e5b
Melody <melody@melody.blue>
2021-10-31 23:14:06 +1000

    Add pre-commit-hooks

--- src/dialNumber.ts
+++ src/dialNumber.ts
@@ -1,52 +1,50 @@
 import {
-    Context,
-    ServerlessCallback,
-    ServerlessFunctionSignature,
-} from '@twilio-labs/serverless-runtime-types/types';
+  Context,
+  ServerlessCallback,
+  ServerlessFunctionSignature,
+} from "@twilio-labs/serverless-runtime-types/types";
 
-const DEFAULT_COUNTRY_CODE = "+61"
+const DEFAULT_COUNTRY_CODE = "+61";
 
 // You have to change that based on which parameters you expect to be passed to
 // your Twilio Function via the POST body or request parameters.
 type RequestParameters = {
-    request: {
-        cookies: {};
-        headers: {};
-    }
-    Digits?: String
+  // eslint-disable-next-line
+  request: { cookies: {}; headers: {} };
+  Digits?: string;
 };
 
 export const handler: ServerlessFunctionSignature = function (
-    context: Context<{ PROXY_PHONE?: String }>,
-    event: RequestParameters,
-    callback: ServerlessCallback
+  context: Context<{ PROXY_PHONE?: string }>,
+  event: RequestParameters,
+  callback: ServerlessCallback
 ) {
-    const twiml = new Twilio.twiml.VoiceResponse();
+  const twiml = new Twilio.twiml.VoiceResponse();
 
-    const proxyPhoneNumber = context.PROXY_PHONE;
-    if (proxyPhoneNumber === undefined) {
-        twiml.say("Unexpected error, we have been alerted.")
-        return callback(null, twiml)
-    }
-    const inputPhoneNumber = event.Digits;
-
-    if (inputPhoneNumber === undefined) {
-        twiml.say(`No phone number provided`);
-        return callback(null, twiml)
-    }
-
-    // Phone Numbers must start with +61
-    let phoneNumberToCall = ""
-    if (inputPhoneNumber[0] === "0") {
-        phoneNumberToCall = DEFAULT_COUNTRY_CODE + inputPhoneNumber.slice(1)
-    } else {
-        phoneNumberToCall = DEFAULT_COUNTRY_CODE + inputPhoneNumber
-    }
-
-    twiml.say(`Starting call to ${phoneNumberToCall.split('').join(' ')}`);
-
-    // callerId required to make the phone call "originate" from proxyPhoneNumber
-    twiml.dial({ callerId: proxyPhoneNumber.toString() }, phoneNumberToCall.toString());
-
+  const proxyPhoneNumber = context.PROXY_PHONE;
+  if (proxyPhoneNumber === undefined) {
+    twiml.say("Unexpected error, we have been alerted.");
     return callback(null, twiml);
+  }
+  const inputPhoneNumber = event.Digits;
+
+  if (inputPhoneNumber === undefined) {
+    twiml.say(`No phone number provided`);
+    return callback(null, twiml);
+  }
+
+  // Phone Numbers must start with +61
+  let phoneNumberToCall = "";
+  if (inputPhoneNumber[0] === "0") {
+    phoneNumberToCall = DEFAULT_COUNTRY_CODE + inputPhoneNumber.slice(1);
+  } else {
+    phoneNumberToCall = DEFAULT_COUNTRY_CODE + inputPhoneNumber;
+  }
+
+  twiml.say(`Starting call to ${phoneNumberToCall.split("").join(" ")}`);
+
+  // callerId required to make the phone call "originate" from proxyPhoneNumber
+  twiml.dial({ callerId: proxyPhoneNumber }, phoneNumberToCall);
+
+  return callback(null, twiml);
 };
--- src/voice.ts
+++ src/voice.ts
@@ -2,49 +2,48 @@ import {
   Context,
   ServerlessCallback,
   ServerlessFunctionSignature,
-} from '@twilio-labs/serverless-runtime-types/types';
+} from "@twilio-labs/serverless-runtime-types/types";
 
 // You have to change that based on which parameters you expect to be passed to
 // your Twilio Function via the POST body or request parameters.
 type RequestParameters = {
-  request: {
-    cookies: {};
-    headers: {};
-  }
-  From?: String
+  // eslint-disable-next-line
+  request: { cookies: {}; headers: {} };
+  From?: string;
 };
 
 export const handler: ServerlessFunctionSignature = function (
-  context: Context<{ ORGANISER_PHONE?: String, PROXY_PHONE?: String }>,
+  context: Context<{ ORGANISER_PHONE?: string; PROXY_PHONE?: string }>,
   event: RequestParameters,
   callback: ServerlessCallback
 ) {
-
   const twiml = new Twilio.twiml.VoiceResponse();
-  console.log(context)
+  console.log(context);
 
   const privatePhoneNumber = context.ORGANISER_PHONE;
   if (privatePhoneNumber === undefined) {
-    twiml.say("Unexpected error, we have been alerted.")
-    return callback(null, twiml)
+    twiml.say("Unexpected error, we have been alerted.");
+    return callback(null, twiml);
   }
 
   const proxyPhoneNumber = context.PROXY_PHONE;
   if (proxyPhoneNumber === undefined) {
-    twiml.say("Unexpected error, we have been alerted.")
-    return callback(null, twiml)
+    twiml.say("Unexpected error, we have been alerted.");
+    return callback(null, twiml);
   }
 
   if (event.From === privatePhoneNumber) {
-    const gather = twiml.gather({ action: '/dialNumber' });
-    gather.say('Welcome to the forwarding dashboard. Please enter a phone number followed by the hash key?');
+    const gather = twiml.gather({ action: "/dialNumber" });
+    gather.say(
+      "Welcome to the forwarding dashboard. Please enter a phone number followed by the hash key?"
+    );
 
     // fallback message in case no response is given
-    twiml.say('We didn\'t receive any input. Goodbye!');
+    twiml.say("We didn't receive any input. Goodbye!");
     return callback(null, twiml);
   } else {
     // forward call to your private phone number
-    twiml.dial({ callerId: proxyPhoneNumber.toString() }, privatePhoneNumber.toString());
+    twiml.dial({ callerId: proxyPhoneNumber }, privatePhoneNumber);
     return callback(null, twiml);
   }
 };
