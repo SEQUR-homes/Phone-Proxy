50e0473d48ba10a9e276f9499dc6bd63c92a49c3
Melody <melody@melody.blue>
2021-10-31 22:50:04 +1000

    Initial Commit

---
+++ src/voice.ts
@@ -0,0 +1,50 @@
+import {
+  Context,
+  ServerlessCallback,
+  ServerlessFunctionSignature,
+} from '@twilio-labs/serverless-runtime-types/types';
+
+// You have to change that based on which parameters you expect to be passed to
+// your Twilio Function via the POST body or request parameters.
+type RequestParameters = {
+  request: {
+    cookies: {};
+    headers: {};
+  }
+  From?: String
+};
+
+export const handler: ServerlessFunctionSignature = function (
+  context: Context<{ ORGANISER_PHONE?: String, PROXY_PHONE?: String }>,
+  event: RequestParameters,
+  callback: ServerlessCallback
+) {
+
+  const twiml = new Twilio.twiml.VoiceResponse();
+  console.log(context)
+
+  const privatePhoneNumber = context.ORGANISER_PHONE;
+  if (privatePhoneNumber === undefined) {
+    twiml.say("Unexpected error, we have been alerted.")
+    return callback(null, twiml)
+  }
+
+  const proxyPhoneNumber = context.PROXY_PHONE;
+  if (proxyPhoneNumber === undefined) {
+    twiml.say("Unexpected error, we have been alerted.")
+    return callback(null, twiml)
+  }
+
+  if (event.From === privatePhoneNumber) {
+    const gather = twiml.gather({ action: '/dialNumber' });
+    gather.say('Welcome to the forwarding dashboard. Please enter a phone number followed by the hash key?');
+
+    // fallback message in case no response is given
+    twiml.say('We didn\'t receive any input. Goodbye!');
+    return callback(null, twiml);
+  } else {
+    // forward call to your private phone number
+    twiml.dial({ callerId: proxyPhoneNumber.toString() }, privatePhoneNumber.toString());
+    return callback(null, twiml);
+  }
+};
